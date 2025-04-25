eab84ef8176a0150dbb4b38a216985c6d27c7f98
mmaybei <yebbi2@ewhain.net>
2025-04-25 14:23:23 +0900

    [feat] 할 일 추가 로직 구현

--- week02-assignment/script.js
+++ week02-assignment/script.js
@@ -5,6 +5,9 @@ const tbody = table.querySelector('tbody');
 const totalButton = document.querySelector(".total-button");
 const completedButton = document.querySelector(".completed-button");
 const incompleteButton = document.querySelector(".incomplete-button");
+const todoInput = document.querySelector(".todo-input");
+const importanceSelect = document.querySelector(".importance-select");
+const addButton = document.querySelector(".add-button");
 
 localStorage.setItem('todos', JSON.stringify(todos));
 const storedTodos = JSON.parse(localStorage.getItem('todos')) || [];
@@ -21,6 +24,29 @@ completedButton.addEventListener('click', () => {
 incompleteButton.addEventListener('click', () => {
     const incompleteTodos = storedTodos.filter(todo => !todo.completed);
     renderTodos(incompleteTodos);
+});
+
+addButton.addEventListener('click', () => {
+    const input = todoInput.value;
+    const select = importanceSelect.value;
+
+    if (input && select) {
+        const newTodo = {
+            id: storedTodos.length + 1,
+            title: input,
+            completed: false,
+            priority: select
+        };
+
+        storedTodos.push(newTodo);
+        localStorage.setItem('todos', JSON.stringify(storedTodos));
+        renderTodos(storedTodos);
+
+        todoInput.value = '';
+        importanceSelect.value = '';
+    } else {
+        alert("할 일과 중요도를 모두 입력해 주세요!");
+    }
 });
 
 function renderTodos(todos) {
